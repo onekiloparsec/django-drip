df3dec78ce7b7c99bfa85348b547729077f7b9fe
Cédric Foellmi <cedric@onekilopars.ec>
2019-07-26 21:56:11 +0200

    Not needed

--- drip/models.py
+++ drip/models.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth import get_user_model
